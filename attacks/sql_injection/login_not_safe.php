51990f4236dcaf0318249b46dbef14cba0abe20b
Lucaswillians <lucaswilianserpa@gmail.com>
2024-04-25 20:03:49 -0300

    third commit

--- attacks/sql_injection/login_not_safe.php
+++ attacks/sql_injection/login_not_safe.php
@@ -1,16 +1,17 @@
 <?php
 // Conectar ao banco de dados SQLite
-$db = new SQLite3('users.db');
+// $db = new SQLite3('users.db');
 
 // Verificar se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Consulta sem verificação adequada dos dados de entrada (mais suscetível a injeção de SQL)
     $sql = "SELECT * FROM users WHERE username='" . $_POST['username'] . "' AND password='" . $_POST['password'] . "'";
-    $result = $db->query($sql);
+    die($sql);
+    // $result = $db->query($sql);
 
-    if ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    if (true) //$row = $result->fetchArray(SQLITE3_ASSOC)) {
         // Usuário autenticado com sucesso
-        echo "Login bem-sucedido! Bem-vindo, " . $row['username'] . ".";
+    echo "Login bem-sucedido!.";
     } else {
         // Nome de usuário ou senha incorretos
         echo "Nome de usuário ou senha incorretos!";
